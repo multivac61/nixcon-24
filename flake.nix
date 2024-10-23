795b5efc35c260c16159f75e0a39b523c5558f92
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-10-23 23:41:38 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              nodePackages.pnpm
+            ];
+          };
+      });
+    };
+}
